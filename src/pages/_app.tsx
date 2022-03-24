84e2db8f239158aa57b961ed437a59841fa50a0d
sam bacha <sam@manifoldfinance.com>
2022-03-24 07:21:42 -0700

    fix ios

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,50 +7,30 @@ import store from "app/state";
 import Layout from "components/Layout";
 import type { AppProps } from "next/app";
 import { Provider as ReduxProvider } from "react-redux";
-import PWAPrompt from 'react-ios-pwa-prompt'
-import { useRouter } from 'next/router'
-import React, { Component, Fragment, useEffect } from 'react'
-import { BrowserRouter, useLocation } from 'react-router-dom'
+import dynamic from 'next/dynamic';
 
-  function MyApp({ Component, pageProps, fallback, err }) {
-    const router = useRouter()
-    const { locale, events } = router
+const PWAPrompt: any = dynamic(() => import('react-ios-pwa-prompt'), {
+  ssr: false,
+});
 
-  function ScrollToTop() {
-    const { pathname } = useLocation()
-
-    useEffect(() => {
-      window.scrollTo(0, 0)
-      /** `
-    
-       if (isProd) {
-        Fathom.trackPageview();
-
-      } */
-    }, [pathname])
-
-    return null
-  }
-  const Index = () => {
-    useEffect(() => {}, [])
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-  <>
-           <PWAPrompt
+    <> 
+    <ReduxProvider store={store}>
+      <Layout>
+      <PWAPrompt
           timesToShow={2}
           permanentlyHideOnDismiss={false}
           copyTitle="Sushi now on iOS"
           copyBody="Sushi works best when added to the homescreen. Tap the share button to add Sushi to your home screen."
           copyClosePrompt="Close"
         />
-    <ReduxProvider store={store}>
-      <Layout>
         <Component {...pageProps} />
       </Layout>
       <Portals />
     </ReduxProvider>
     </>
-    )
-  }
+  );
 }
 
-export default MyApp
+export default MyApp;
